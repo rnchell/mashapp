93e91390565b47180ddcece7e242876ef41843d9
Buddy <kaladin@Raymonds-MacBook-Pro.local>
2014-02-17 22:04:46 -0800

    fixed profile pic

--- public/scripts/mash.js
+++ public/scripts/mash.js
@@ -1,12 +1,3 @@
-// //Date Model
-function DateModel(createdAt, dateTime, place, participants, acceptCount) {
-	this.createdAt = createdAt;
-	this.dateTime = dateTime;
-	this.place = place;
-	this.participants = participants;
-	this.acceptCount = acceptCount;
-}
-
 function View(templateName, data) {
   this.templateName = templateName;
   this.data = data;
@@ -23,10 +14,6 @@ var proposalViewModel = function(participants, time, location, message, matchmak
   self.location = ko.observable(location);
   self.message = ko.observable(message);
 };
-
-// var usersViewModel = {
-// 	users: ko.observableArray()
-// };
 
 var usersViewModel = function(){
   var self = this;
@@ -62,39 +49,6 @@ var accountViewModel = function(user){
   var self = this;
 
   self.user = user;
-  self.photo = user.photo;
+  self.photo = 'url(' + user.photo + ')';
   self.isUserLoggedIn = true;
 };
-
-$(function() {
-  //ko.applyBindings(new accountViewModel(), document.getElementById('account-view'));  
-});
-
-//$(function() {
-  //ko.applyBindings(viewModel, document.getElementById('content'));
-//});
-//$(function () {
-
-	// $.get('/friends', function(data) {
-	// 	usersModel.users = ko.observableArray(data.friends);
-	// 	viewModel.selectedView(new View("userTemplate", usersModel));
-	// });
-
-  //ko.applyBindings(viewModel, document.getElementById('content'));
-//});
-
-//maybe i put this in the viewModel because it will be used to navigate
-//between views and to change the selectedView context
-// 	self.dateHolder = [];
-// 	self.match = function(user) {
-// 		if (self.dateHolder.length === 0) {
-//       console.log("first to be asked");
-//       self.dateHolder.push(user);
-// 		} else if (self.dateHolder[0] === user){
-//       self.dateHolder.pop();
-// 		} else {
-// 			var dateModel = new DateModel(new Date(), "Wed, March 5 at 7pm", "1335 Filbert Street", [], []);
-// 			var dateProposalUI = new DateProposalUI(dateModel); 
-// 		}
-// 	}
-// }
