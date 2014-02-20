57f030b9395ab6e23f5e577e854c95c50a448cbd
bzoo <bczulauf@gmail.com>
2014-02-20 09:56:46 -0800

    starting down path of update and delete for users

---
+++ public/viewModels/viewModel.js
@@ -0,0 +1,23 @@
+function View(templateName, data) {
+  this.templateName = templateName;
+  this.data = data;
+};
+
+function ViewModel() {
+  var self = this;
+  self.isUserLoggedIn = ko.observable(false);
+  self.isUserModelCreated = ko.observable(false);
+  self.selectedView = ko.observable();
+  self.goToHomeView = function () {
+    self.selectedView({templateName: "friendsTemplate", data: friendsViewModel}); 
+  };
+  self.goToDatesView = function () {
+    self.selectedView({templateName: "datesTemplate", data: userViewModel})
+  }
+};
+
+var viewModel = new ViewModel();
+
+$(function() {
+  ko.applyBindings(viewModel, document.getElementById('container'));
+});
